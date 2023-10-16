1396d7eb5e1d10c79ab6d9bb05baae475f406bd2
Alexandre Fouche <alexandre.fouche@gmail.com>
2023-10-16 18:58:35 +0200

    Merge branch 'rocky9'

--- manifests/plugin/nginx.pp
+++ manifests/plugin/nginx.pp
@@ -23,14 +23,21 @@ class letsencrypt::plugin::nginx (
       },
     }
 
-    file { '/etc/letsencrypt/options-ssl-nginx.conf':
-      ensure  => link,
-      target  => $::osfamily ? {
-        'Redhat' => '/usr/lib/python3.6/site-packages/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf',
-        'Debian' => '/usr/lib/python3/dist-packages/certbot_nginx/options-ssl-nginx.conf',
-      },
-      require => Package[$package_name],
-    }
-  }
+    # $python_version = $operatingsystemmajrelease ? {
+    #     '8'     => '3.6',
+    #     '9'     => '3.9',
+    #     default => undef,
+    # }
 
-}
+    # file { '/etc/letsencrypt/options-ssl-nginx.conf':
+    #   ensure  => link,
+    #   target  => $::osfamily ? {
+    #     'Redhat' => "/usr/lib/python${python_version}/site-packages/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf",
+    #     'Debian' => '/usr/lib/python3/dist-packages/certbot_nginx/options-ssl-nginx.conf',
+    #   },
+    #   require => Package[$package_name],
+    # }
+
+  }  # if $manage_package
+
+}  # class letsencrypt::plugin::nginx
